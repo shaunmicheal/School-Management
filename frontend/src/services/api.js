e9fc581d5fa1b1d065ee09cd399a7218d562832c
shaunmicheal <shaunjeranyama@gmail.com>
2026-08-31 23:59:30 +0200

    notifications fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://school-management-7e1c.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
@@ -15,7 +15,7 @@ API.interceptors.request.use(
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
 export default API;
